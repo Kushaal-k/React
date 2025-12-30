a3e507083adb94a0ff78354c2399bd0ab4b1f0a3
Kushaal Kankane <kushaalkankane12345@gmail.com>
2025-12-30 07:59:46 +0000

    Configured Redux Toolikit

--- MegaProject/src/App.jsx
+++ MegaProject/src/App.jsx
@@ -1,14 +1,43 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/authService.js";
+import { login, logout } from "./store/authSlice.js";
+import {Header, Footer} from './components/index.js';
+import "./App.css";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  
+	const [loading, setLoading] = useState(true);
+	const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>A Mega Project</h1>
-    </>
-  )
+	useEffect(() => {
+		authService.getCurrentUser()
+			.then((userData) => {
+				if (userData) {
+					dispatch(login({ userData }));
+				} 
+        else {
+					dispatch(logout());
+				}
+			})
+			.catch((error) =>
+				console.log("Error while fetching user data ", error)
+			)
+			.finally(() => setLoading(false));
+	}, []);
+
+	return !loading ? 
+    (
+      <div className="min-h-screen flex flex-wrap content-between bg-gray-50">
+        <div className="w-full block">
+          <Header />
+          <main>
+          // TODO: <Outlet />
+          </main>
+          <Footer />
+        </div>
+      </div>
+    ) : null;
 }
 
-export default App
+export default App;
